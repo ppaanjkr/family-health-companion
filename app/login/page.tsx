c546b04b74aef7f342643752b94f736c1092a5e8
ppaanjkr <88655177+ppaanjkr@users.noreply.github.com>
2026-08-02 19:34:14 +0700

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,16 +7,13 @@ export default function LoginPage() {
     <main className="flex flex-1 items-center justify-center bg-slate-50 px-4 py-10">
       <Card  className="w-full max-w-md p-8 ring-1 ring-slate-200 sm:p-10">
         <p className="text-sm font-semibold tracking-wide text-emerald-600">
-          FAMILY HEALTH COMPANION
+          4Ducks
         </p>
-        <h1 className="mt-3 text-3xl font-semibold tracking-tight text-slate-900">
-          สุขภาพของครอบครัว
-          <br />
-          อยู่ใกล้มือเสมอ
+        <h1 className="mt-3 text-md font-semibold tracking-tight text-slate-900">
+          บันทึกสุขภาพ & บันทึกค่าใช้จ่าย
         </h1>
-        <p className="mt-4 text-sm leading-6 text-slate-600">
-          เข้าสู่ระบบด้วยบัญชี LINE
-          เพื่อเริ่มบันทึกและติดตามข้อมูลสุขภาพของคนที่คุณรัก
+        <p className="mt-4 text-xs leading-6 text-slate-600">
+          เข้าสู่ระบบด้วยบัญชี LINE กดปุ่มเขียวข้างล่าง ละมาบอกอิฉัน
         </p>
         <form
           className="mt-8"
